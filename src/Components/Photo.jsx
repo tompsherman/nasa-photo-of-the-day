457556120866f72ac2f0cae98c40a5da5e658c87
Tom Sherman <tompsherman@gmail.com>
2020-09-10 16:28:04 -0400

    styling flex, center, padding

--- src/Components/Photo.jsx
+++ src/Components/Photo.jsx
@@ -1,14 +1,25 @@
 import React, { useState, useEffect} from 'react'
+import styled, { keyframes } from "styled-components";
+
+const StyledPhoto = styled.div`
+display: flex;
+flex-direction: column;
+align-content: center;
+max-width: 100%;
+padding: 2%;
+background-color: ${props => props.theme.black};
+color: ${props=> props.theme.white};
+`
 
 const Photo = ({apod, title}) => {
   console.log(apod.title)
-return (<div>
-    <h2>{title}</h2>
+return (
+   <StyledPhoto><h2>{title}</h2>
     <h2>{apod.title}</h2>
 <h3>{apod.date}</h3>
     <img src={apod.url} alt="NASA pic of day"/>
 <p>{apod.explanation}</p>
- </div>)
+</StyledPhoto>)
 }
 
 export default Photo
---
+++ src/theme/index.js
@@ -0,0 +1,11 @@
+export default {
+    primaryColor: "royalblue",
+    secondaryColor: "goldenrod",
+    tertiaryColor: "mediumseagreen",
+    white: "smokewhite",
+    black: "black",
+    mobileBreakpoint: "(max-width: 500 px)",
+    border: "1px solid smokewhite"
+
+
+}
